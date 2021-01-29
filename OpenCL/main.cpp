56d71ef66508d97385132a306fdff0975d280929
dmitrryy <da.drozdov@mail.ru>
2021-01-29 17:12:37 +0300

    added bugs

--- OpenCL/main.cpp
+++ OpenCL/main.cpp
@@ -7,9 +7,9 @@
 #include <memory>
 #include <cstring>
 
-#include <CL/cl.h>
 
-#include <CL/opencl.h>
+#include "Wrapper.h"
+
 #ifdef DEBUG
 
 #define LOG_OCL_COMPILER_ERROR(PROGRAM, DEVICE)\
@@ -101,28 +101,29 @@ int main() {
     clStatus = clGetDeviceIDs(platforms[0], CL_DEVICE_TYPE_GPU, num_device, device_list.data(), nullptr);
 
     // Create one OpenCL context for each device in the platform
-    cl_context context = clCreateContext(nullptr, num_device, device_list.data(), nullptr, nullptr, &clStatus);
+    ezg::Wrapper< cl_context > context(clCreateContext(nullptr, num_device, device_list.data(), nullptr, nullptr, &clStatus));
 
     // Create a command queue
-    cl_command_queue commandQueue = clCreateCommandQueue(context, device_list[0], 0, &clStatus);
+    ezg::Wrapper< cl_command_queue > commandQueue(clCreateCommandQueue(context.get(), device_list[0], 0, &clStatus));
 
     // Create memory buffers on the device for each vector
-    cl_mem A_clmem = clCreateBuffer(context, CL_MEM_READ_ONLY, size_vec * sizeof(float), nullptr, &clStatus);
+    ezg::Wrapper< cl_mem > A_clmem(clCreateBuffer(context.get(), CL_MEM_READ_ONLY, size_vec * sizeof(float), nullptr, &clStatus));
 
     // Copy the Buffer A and B to the device
-    clStatus = clEnqueueWriteBuffer(commandQueue, A_clmem, CL_TRUE, 0, size_vec * sizeof(float), data.data(), 0,
+    clStatus = clEnqueueWriteBuffer(commandQueue.get(), A_clmem.get(), CL_TRUE, 0, size_vec * sizeof(float), data.data(), 0,
                                     nullptr, nullptr);
 
     std::string sax_kernel = readFile("kernel.cl");
     const char *pSaxKernel = sax_kernel.data();
     // Create a program from the kernel source
-    cl_program program = clCreateProgramWithSource(context, 1, &pSaxKernel, nullptr, &clStatus);
+    ezg::Wrapper< cl_program > program(clCreateProgramWithSource(context.get(), 1, &pSaxKernel, nullptr, &clStatus));
 
     // Build the program
-    clStatus = clBuildProgram(program, 1, device_list.data(), nullptr, nullptr, nullptr);
-    LOG_OCL_COMPILER_ERROR(program, device_list[0]);
+    clStatus = clBuildProgram(program.get(), 1, device_list.data(), nullptr, nullptr, nullptr);
+    LOG_OCL_COMPILER_ERROR(program.get(), device_list[0]);
+
     // Create the OpenCL kernel
-    cl_kernel kernel = clCreateKernel(program, "bitonic_sort_kernel", &clStatus);
+    ezg::Wrapper< cl_kernel > kernel(clCreateKernel(program.get(), "bitonic_sort_kernel", &clStatus));
 
     size_t global_size = size_vec / 2;
     // This is the size of the work group.
@@ -131,17 +132,17 @@ int main() {
     size_t num_of_work_groups = global_size / local_size;
 
 
-    DATA_TYPE *mapped_input_buffer = (DATA_TYPE *) clEnqueueMapBuffer(commandQueue, A_clmem, true, CL_MAP_READ, 0,
+    DATA_TYPE *mapped_input_buffer = (DATA_TYPE *) clEnqueueMapBuffer(commandQueue.get(), A_clmem.get(), true, CL_MAP_READ, 0,
                                                                       sizeof(DATA_TYPE) * size_vec, 0, NULL, NULL,
                                                                       &clStatus);
     // Display the Sorted data on the screen
     for (int i = 0; i < size_vec; i++)
         printf("%d ", mapped_input_buffer[i]);
     fflush(stdout);
-    clEnqueueUnmapMemObject(commandQueue, A_clmem, mapped_input_buffer, 0, nullptr, nullptr);
+    clEnqueueUnmapMemObject(commandQueue.get(), A_clmem.get(), mapped_input_buffer, 0, nullptr, nullptr);
 
 
-    clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *) &A_clmem);
+    clSetKernelArg(kernel.get(), 0, sizeof(cl_mem), (void *) &A_clmem);
     unsigned int stage, passOfStage, numStages, temp;
     stage = passOfStage = numStages = 0;
     for (temp = size_vec; temp > 1; temp >>= 1)
@@ -150,22 +151,22 @@ int main() {
     local_size = global_size;
     for (stage = 0; stage < numStages; ++stage) {
         // stage of the algorithm
-        clSetKernelArg(kernel, 1, sizeof(int), (void *) &stage);
+        clSetKernelArg(kernel.get(), 1, sizeof(int), (void *) &stage);
         // Every stage has stage + 1 passes
         for (passOfStage = 0; passOfStage < stage + 1; ++passOfStage) {
             // pass of the current stage
             std::cout << "Pass no " << passOfStage << std::endl;
-            clStatus = clSetKernelArg(kernel, 2, sizeof(int), (void *) &passOfStage);
+            clStatus = clSetKernelArg(kernel.get(), 2, sizeof(int), (void *) &passOfStage);
             //
             // Enqueue a kernel run call.
             // Each thread writes a sorted pair.
             // So, the number of threads (global) should be half the length of the input buffer.
 
-            clEnqueueNDRangeKernel(commandQueue, kernel, 1, nullptr, &global_size, &local_size, 0, NULL, NULL);
+            clEnqueueNDRangeKernel(commandQueue.get(), kernel.get(), 1, nullptr, &global_size, &local_size, 0, NULL, NULL);
 
-            clFinish(commandQueue);
+            clFinish(commandQueue.get());
 
-            DATA_TYPE *mapped_input_buffer = (DATA_TYPE *) clEnqueueMapBuffer(commandQueue, A_clmem, true, CL_MAP_READ, 0,
+            DATA_TYPE *mapped_input_buffer = (DATA_TYPE *) clEnqueueMapBuffer(commandQueue.get(), A_clmem.get(), true, CL_MAP_READ, 0,
                                                                               sizeof(DATA_TYPE) * size_vec, 0, NULL, NULL,
                                                                               &clStatus);
             // Display the Sorted data on the screen
@@ -173,17 +174,9 @@ int main() {
                 printf("%d ", mapped_input_buffer[i]);
             fflush(stdout);
 
-            clEnqueueUnmapMemObject(commandQueue, A_clmem, mapped_input_buffer, 0, nullptr, nullptr);
+            clEnqueueUnmapMemObject(commandQueue.get(), A_clmem.get(), mapped_input_buffer, 0, nullptr, nullptr);
         }//end of for passStage = 0:stage-1
     }//end of for stage = 0:numStage-1
-
-
-    // Finally release all OpenCL allocated objects and host buffers.
-    clStatus = clReleaseKernel(kernel);
-    clStatus = clReleaseProgram(program);
-    clStatus = clReleaseMemObject(A_clmem);
-    clStatus = clReleaseCommandQueue(commandQueue);
-    clStatus = clReleaseContext(context);
 
     return 0;
 }
